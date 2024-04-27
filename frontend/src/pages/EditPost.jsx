f6a9521ce4998381fef074fb9cf698ea22358136
ParadoxNJ005 <140380753+ParadoxNJ005@users.noreply.github.com>
2024-04-27 10:18:16 +0530

    Update EditPost.jsx

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -112,7 +112,7 @@ const EditPost = () => {
               />
               <div
                 onClick={addCategory}
-                className="bg-black text-white px-4 py-2 font-semibold cursor-pointer"
+                className="text-[#F8F0E3] bg-[#88881D] px-4 py-2 font-semibold cursor-pointer"
               >
                 Add
               </div>
@@ -146,7 +146,7 @@ const EditPost = () => {
           />
           <button
             onClick={handleUpdate}
-            className="bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg"
+            className="text-[#F8F0E3] bg-[#88881D] w-full md:w-[20%] mx-auto font-semibold px-4 py-2 md:text-xl text-lg"
           >
             Update
           </button>
